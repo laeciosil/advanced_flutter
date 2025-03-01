a6b5f8ba30b78a2f387b1ecfcabd53d1aed6c0ff
Laecio Silva <laecio12silva@gmail.com>
2025-03-01 15:27:34 -0300

    Refactor NextEventPlayer to use a factory constructor for initialization and make initials a final field

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -1,26 +1,42 @@
 import 'package:flutter_test/flutter_test.dart';
 
 class NextEventPlayer {
-  NextEventPlayer({
+  NextEventPlayer._({
     required this.id,
     required this.name,
     required this.isConformed,
+    required this.initials,
     this.confirmationDate,
     this.photo,
     this.position,
-  }) {
-    initials = _getInitials();
-  }
+  });
+
+  factory NextEventPlayer({
+    required String id,
+    required String name,
+    required bool isConformed,
+    String? photo,
+    String? position,
+    DateTime? confirmationDate,
+  }) =>
+      NextEventPlayer._(
+          id: id,
+          name: name,
+          photo: photo,
+          position: position,
+          initials: _getInitials(name),
+          isConformed: isConformed,
+          confirmationDate: confirmationDate);
 
   final String id;
   final String name;
   final String? photo;
   final String? position;
   final bool isConformed;
-  late final String initials;
+  final String initials;
   final DateTime? confirmationDate;
 
-  String _getInitials() {
+  static String _getInitials(String name) {
     final names = name.split(' ');
 
     final firstChar = names.first[0];
