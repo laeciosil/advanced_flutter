805a8b0e3098d43940d1f327cf717f6547d61854
Laecio Silva <laecio12silva@gmail.com>
2025-03-04 09:05:22 -0300

    Implement LoadNextEventHttpRepository to return NextEvent and update tests

--- test/infra/repositories/load_next_event_http_repo_test.dart
+++ test/infra/repositories/load_next_event_http_repo_test.dart
@@ -2,13 +2,16 @@ import 'dart:convert';
 
 import 'dart:typed_data';
 
+import 'package:advanced_flutter/domain/entities/next_event.dart';
+import 'package:advanced_flutter/domain/entities/next_event_player.dart';
+import 'package:advanced_flutter/domain/repositories/load_next_events_repository.dart';
 import 'package:http/http.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../helpers/fakes.dart';
 
-class LoadNextEventHttpRepository {
+class LoadNextEventHttpRepository implements LoadedNextEventRepository {
   LoadNextEventHttpRepository({
     required this.httpClient,
     required this.url,
@@ -17,13 +20,34 @@ class LoadNextEventHttpRepository {
   final Client httpClient;
   final String url;
 
-  Future<void> loadNextEvent({required String groupId}) async {
+  @override
+  Future<NextEvent> loadNextEvent({required String groupId}) async {
     final uri = Uri.parse(url.replaceFirst(':groupId', groupId));
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await httpClient.get(uri, headers: headers);
+    final response = await httpClient.get(uri, headers: headers);
+
+    final event = jsonDecode(response.body);
+
+    return NextEvent(
+      groupName: event['groupName'],
+      date: DateTime.parse(event['date']),
+      players: event['players']
+          .map<NextEventPlayer>(
+            (player) => NextEventPlayer(
+              id: player['id'],
+              name: player['name'],
+              isConformed: player['isConfirmed'],
+              confirmationDate:
+                  DateTime.tryParse(player['confirmationDate'] ?? ''),
+              photo: player['photo'],
+              position: player['position'],
+            ),
+          )
+          .toList(),
+    );
   }
 }
 
@@ -31,6 +55,8 @@ class HttpClientSpy implements Client {
   String? method;
   int callsCount = 0;
   String? url;
+  String responseJson = '';
+  int statusCode = 200;
   Map<String, String>? headers;
 
   @override
@@ -48,7 +74,7 @@ class HttpClientSpy implements Client {
     callsCount++;
     this.url = url.toString();
     this.headers = headers;
-    return Response('', 200);
+    return Response(responseJson, statusCode);
   }
 
   @override
@@ -99,6 +125,25 @@ void main() {
     groupId = anyString();
     const url = 'https//domain.com/api/groups/:groupId/next_event';
     httpClient = HttpClientSpy();
+    httpClient.responseJson = '''
+      {
+      "groupName": "any name",
+      "date": "2025-03-10T15:30",
+      "players": [{
+        "id": "id 1",
+        "name": "name 1",
+        "isConfirmed": true
+      }, {
+        "id": "id 2",
+        "name": "name 2",
+        "position": "position 2",
+        "photo": "photo 2",
+        "confirmationDate": "2025-03-10T15:30",
+        "isConfirmed": false      
+      }]
+      
+      }
+    ''';
     sut = LoadNextEventHttpRepository(httpClient: httpClient, url: url);
   });
 
@@ -120,5 +165,22 @@ void main() {
 
     expect(httpClient.headers?['content-type'], 'application/json');
     expect(httpClient.headers?['accept'], 'application/json');
+  });
+
+  test('should return NextEvent on 200', () async {
+    final event = await sut.loadNextEvent(groupId: groupId);
+
+    expect(event.groupName, 'any name');
+    expect(event.date, DateTime(2025, 3, 10, 15, 30));
+    expect(event.players[0].id, 'id 1');
+    expect(event.players[0].name, 'name 1');
+    expect(event.players[0].isConformed, true);
+
+    expect(event.players[1].id, 'id 2');
+    expect(event.players[1].name, 'name 2');
+    expect(event.players[1].isConformed, false);
+    expect(event.players[1].position, 'position 2');
+    expect(event.players[1].photo, 'photo 2');
+    expect(event.date, DateTime(2025, 3, 10, 15, 30));
   });
 }
