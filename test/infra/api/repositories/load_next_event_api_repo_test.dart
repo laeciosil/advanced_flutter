d2d3d7a28a3ab69e71bf4cbe37ed01c5a29465be
Laecio Silva <laecio12silva@gmail.com>
2025-03-15 07:34:47 -0300

    Add LoadNextEventApiRepository and corresponding tests for HTTP client interactions

---
+++ test/infra/api/repositories/load_next_event_api_repo_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../helpers/fakes.dart';
+
+class LoadNextEventApiRepository {
+  final HttpGetClient httpClient;
+  final String url;
+
+  LoadNextEventApiRepository({required this.httpClient, required this.url});
+
+  Future<void> loadNextEvent({required String groupId}) async {
+    await httpClient.get(url: url);
+  }
+}
+
+abstract class HttpGetClient {
+  Future<void> get({required String url});
+}
+
+class HttpGetClientSpy implements HttpGetClient {
+  String? url;
+  int callsCount = 0;
+
+  @override
+  Future<void> get({required String url}) async {
+    this.url = url;
+    callsCount++;
+  }
+}
+
+void main() {
+  late HttpGetClientSpy httpClient;
+  late LoadNextEventApiRepository sut;
+  late String url;
+  late String groupId;
+
+  setUp(() {
+    groupId = anyString();
+    url = anyString();
+    httpClient = HttpGetClientSpy();
+
+    sut = LoadNextEventApiRepository(httpClient: httpClient, url: url);
+  });
+
+  test('should call HttpClient with correct URL', () async {
+    await sut.loadNextEvent(groupId: groupId);
+
+    expect(httpClient.url, url);
+    expect(httpClient.callsCount, 1);
+  });
+}
